822792f2da84053ff50ad6b911c109dbcdd89302
Carlos Santos <carloseliesersantos@gmail.com>
2026-01-13 17:02:11 -0500

    chore: run formatter

--- components/ui/player-aware-flash-list.tsx
+++ components/ui/player-aware-flash-list.tsx
@@ -9,10 +9,7 @@ import { StyleSheet, type ViewStyle } from 'react-native';
 import { FlashList, type FlashListProps } from '@shopify/flash-list';
 import { usePlayerPadding } from '@/hooks/use-player-padding';
 
-export function PlayerAwareFlashList<T>({
-	contentContainerStyle,
-	...props
-}: FlashListProps<T>) {
+export function PlayerAwareFlashList<T>({ contentContainerStyle, ...props }: FlashListProps<T>) {
 	const playerPadding = usePlayerPadding();
 
 	const combinedStyle = {
